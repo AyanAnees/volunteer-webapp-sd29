5d6c12489bca98e4398f7aba5007d4a4f6755357
haleytri <124080734+haleytri@users.noreply.github.com>
2025-03-05 13:08:06 -0600

    relocation of js files

---
+++ server/match.js
@@ -0,0 +1,43 @@
+function matchingEvents(){
+    let findName = document.getElementById("name").value;
+    let names = ["Name Name"];
+    let opts = ["event 1 - info", "event 2 - info"];
+    let nameFound = false;
+    let skills = ["skill1", "skill2"];
+    let prefs = ["pref1", "pref2"]
+    
+    for (let name in names){
+        if (findName == names[name]){
+            nameFound = true;
+        }
+    }
+    if(nameFound == true){
+        for (let skill in skills){
+            document.getElementById("skills").innerText="Skills: " + skills.join(", ");
+        }
+        for (let pref in prefs){
+            document.getElementById("prefs").innerText = "Preferences: " + prefs.join(", ");
+        }
+        let choices = document.getElementById("evSelector");
+        choices.innerHTML="";
+        for (let opt in opts){
+            let option = document.createElement("option");
+            option.textContent = opts[opt];
+            option.value = opts[opt];
+            choices.appendChild(option);
+        }
+    }
+    else{
+        alert("Cannot Find Volunteer Name.");
+    }
+}
+
+function sendEvent(){
+    let eventMatched = document.getElementById("evSelector").value;
+    if(eventMatched){
+        alert("Matched Event Submitted.");
+    }
+    else{
+        alert("No Event Selected.");
+    }
+}
