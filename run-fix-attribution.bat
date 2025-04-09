f6e0914eb831a72290fadd6e1906e756e9a2fb48
haleytri <haley.trinh@gmail.com>
2025-04-08 22:14:58 -0500

    Add bash script with proper formatting for fixing attribution

---
+++ run-fix-attribution.bat
@@ -0,0 +1,14 @@
+@echo off
+echo Running Git attribution fix script...
+echo.
+echo This script will fix all contributor attribution issues.
+echo.
+echo Please ensure Git Bash is installed and in your PATH.
+echo.
+echo Running script...
+
+"C:\Program Files\Git\bin\bash.exe" -c "./fix-attribution.sh"
+
+echo.
+echo Done.
+pause
